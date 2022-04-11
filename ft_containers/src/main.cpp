0e39e6c8525f3afe1b2616ad864cb5b8c23e79d8
Jules FEUILLATRE <jfeuilla@e2r10p4.clusters.42paris.fr>
2022-04-11 15:04:28 +0200

    ok

--- ft_containers/src/main.cpp
+++ ft_containers/src/main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
+#include <string>
+#include <deque>
 
 #ifdef STD
 	std::string rd = "std_out";
+    #include <map>
+	#include <stack>
+	#include <vector>
+	namespace ft = std;
 #else
 	std::string rd = "ft_out";
+	#include "../inc/Vector.hpp"
 #endif
 
 void test(void)
 {
-    std::cout << "hello" << rd << std::endl; 
+    std::cout << "hello" << std::endl;
+    ft::vector<int> vector_int;
+    for(int i = 0; i < 10; i++)
+        vector_int.push_back(i);
+    for(unsigned long i = 0; i < vector_int.size(); i++)
+        std::cout << vector_int[i];
 }
 
 int main(void)
