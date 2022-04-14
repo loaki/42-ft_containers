2cb5f17ff16400e109e10916a72cc73c752b6834
Jules FEUILLATRE <jfeuilla@e3r12p10.clusters.42paris.fr>
2022-04-14 18:03:43 +0200

    ok

--- ft_containers/src/main.cpp
+++ ft_containers/src/main.cpp
@@ -4,44 +4,14 @@
 #include <string>
 #include <deque>
 
+
+#include "../test/vector_test.hpp"
+
 #ifdef STD
 	std::string rd = "std_out";
-    #include <map>
-	#include <stack>
-	#include <vector>
-	namespace ft = std;
 #else
 	std::string rd = "ft_out";
-	#include "../inc/Vector.hpp"
 #endif
-
-void test(void)
-{
-    std::cout << "hello" << std::endl;
-
-    std::cout << "### vector ###" << std::endl;
-    ft::vector<int> vector_a;
-    ft::vector<int> vector_b;
-    for(int i = 0; i < 10; i++)
-        vector_a.push_back(i);
-    vector_a.swap(vector_b);
-    std::cout << "vector a      : ";
-    for(unsigned long i = 0; i < vector_a.size(); i++)
-        std::cout << vector_a[i];
-    std::cout << std::endl;
-    std::cout << "vector b      : ";
-    for(unsigned long i = 0; i < vector_b.size(); i++)
-        std::cout << vector_b[i];
-    std::cout << std::endl;
-    std::cout << "b at(2)       : " << vector_b.at(2) << std::endl;
-    std::cout << "a begin = end : " << (vector_a.begin() == vector_a.end()) << std::endl;
-    std::cout << "b begin = end : " << (vector_b.begin() == vector_b.end()) << std::endl;
-    std::cout << "a empty       : " << vector_a.empty() << std::endl;
-    std::cout << "b empty       : " << vector_b.empty() << std::endl;
-    std::cout << "a size        : " << vector_a.size() << std::endl;
-    std::cout << "b size        : " << vector_b.size() << std::endl;
-    
-}
 
 int main(void)
 {
@@ -52,7 +22,7 @@ int main(void)
 	std::ofstream rdout(rd.c_str());
     std::cout.rdbuf(rdout.rdbuf());
 
-    test();
+    vector_test();
 
 	rdout.close();
     std::cout.rdbuf(coutbuf);
---
+++ ft_containers/test/vector_test.hpp
@@ -0,0 +1,65 @@
+#ifndef VECTOR_TEST_HPP
+# define VECTOR_TEST_HPP
+
+# ifdef STD
+    #include <map>
+	#include <stack>
+	#include <vector>
+	namespace ft = std;
+# else
+	#include "../inc/Vector.hpp"
+# endif
+
+
+template<typename V>
+void display_vector(V vector)
+{
+    std::cout << "vector      : ";
+    for(unsigned long i = 0; i < vector.size(); i++)
+        std::cout << vector[i];
+    std::cout << std::endl;
+    try {
+        std::cout << "at(0)       : " << vector.at(0) << std::endl;
+    } catch (...) {
+        std::cout << "index 0 out of range" << std::endl;
+    }
+    std::cout << "begin = end : " << (vector.begin() == vector.end()) << std::endl;
+    std::cout << "empty       : " << vector.empty() << std::endl;
+    std::cout << "size        : " << vector.size() << std::endl;
+}
+
+void vector_test(void)
+{
+    std::cout << "### vector ###" << std::endl;
+    ft::vector<int> vector_a;
+    ft::vector<int> vector_b;
+    ft::vector<std::string> vector_c;
+    for(int i = 0; i < 10; i++)
+        vector_a.push_back(i);
+    vector_c.push_back("hello");
+    vector_c.push_back("world");
+    vector_a.pop_back();
+    vector_a.swap(vector_b);
+    display_vector(vector_a);
+    std::cout << std::endl;
+    display_vector(vector_b);
+    std::cout << std::endl;
+    vector_b.resize(3);
+    display_vector(vector_b);
+    std::cout << std::endl;
+    std::cout << "vector      : ";
+    for(unsigned long i = 0; i < vector_c.size(); i++)
+        std::cout << vector_c[i];
+    std::cout << std::endl;
+    try {
+        std::cout << "at(0)       : " << vector_c.at(0) << std::endl;
+    } catch (...) {
+        std::cout << "index 0 out of range" << std::endl;
+    }
+    std::cout << "begin = end : " << (vector_c.begin() == vector_c.end()) << std::endl;
+    std::cout << "empty       : " << vector_c.empty() << std::endl;
+    std::cout << "size        : " << vector_c.size() << std::endl;    std::cout << std::endl;
+    std::cout << std::endl;
+}
+
+#endif
